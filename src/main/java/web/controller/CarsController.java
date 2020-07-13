27d355c431e41befd62bf2c9bfda250410fdfdd9
krasnovss91 <krasnovstanislav91@gmail.com>
2020-07-13 15:21:45 +0400

    Locale_1

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -1,12 +1,10 @@
 package web.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import web.containers.CarListContainer;
 import web.models.Car;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,15 +13,15 @@ import java.util.Locale;
 
 @Controller
 public class CarsController {
-   // @GetMapping(value = "cars?locale")
     @GetMapping(value = "cars")
-    public String printCars(ModelMap model) {
+    public String printCars(@RequestParam Locale locale ,ModelMap model) {
+       // @RequestParam Locale locale = new Locale("");
      //   Locale en = new Locale("en");
       //  Locale ru = new Locale("ru");
         String en = "CARS";
         String ru = "МАШИНЫ";
 
-        Locale locale = new Locale("");
+     //   Locale locale = new Locale("");
 
         //model.addAttribute("locale",locale);
 
@@ -42,8 +40,8 @@ public class CarsController {
         //model.addAttribute("cars",listContainer.getCars());
      //   model.addAttribute("cars",listContainer);
         //model.addAttribute("cars",getListOfCars());
+       // return "cars";
         return "cars";
-       // return "cars?locale";
 
     }
     private List<Car> getListOfCars(){
